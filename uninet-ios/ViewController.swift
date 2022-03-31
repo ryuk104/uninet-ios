5375d0433d0a96889bb51f95fd931f2469c40c6a
Alex Ma <66192750+RyukCEO@users.noreply.github.com>
2022-03-31 12:50:44 -0700

    Initial Commit

---
+++ uninet-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uninet-ios
+//
+//  Created by Alex Ma on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
